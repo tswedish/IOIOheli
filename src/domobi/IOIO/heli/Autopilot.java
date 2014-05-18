b6404c1a51ec1f2f0600aa1da4a87dc9c72988dd
tswedish <tristan.swedish@gmail.com>
2014-05-18 11:28:07 -0400

    fix autopilot windup bug

--- src/domobi/IOIO/heli/Autopilot.java
+++ src/domobi/IOIO/heli/Autopilot.java
@@ -22,7 +22,7 @@ public class Autopilot {
 	
 	public float kPt = 0.3f, kIt = 0.001f, kDt = 2.5f;
 	// public float kPr = 0.3f, kIr = 0.0f, kDr = 0.0f;
-	public float kPr = 0.001f, kIr = 0.0f, kDr = 0.0f;
+	public float kPr = 0.0001f, kIr = 0.0f, kDr = 0.0f;
 
 	public float uPt = 0.0f, uIt = 0.0f, uDt = 0.0f;
 	public float uPr = 0.0f, uIr = 0.0f, uDr = 0.0f;
@@ -92,7 +92,7 @@ public class Autopilot {
 		heliModel.setRotationPwr(desiredOrientation.getYaw());
 		
 		// Should be reversed?
-		uPr = -yawError;
+		uPr = yawError;
 
 		uIr = (uIr + yawError * dt);
 
@@ -102,7 +102,7 @@ public class Autopilot {
 		if (rotPower > 1)	{ rotPower = 1; }
 		if (rotPower < -1)  { rotPower = -1; }
 		if (heliModel.getMainPwr() > 0.2f) {
-			heliModel.setRotationPwr(rotPower);
+			heliModel.setRotationPwr(-rotPower);
 		} else	{
 			rotPower = 0.0f;
 		}
